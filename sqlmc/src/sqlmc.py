f996d26029507cf232c92c96f3e8b07eb6b34cb3
malvads <malvarez@redborder.com>
2024-05-15 00:04:22 +0100

    The license haha

--- sqlmc/src/sqlmc.py
+++ sqlmc/src/sqlmc.py
@@ -1,3 +1,15 @@
+
+###################################################################
+# This file is licensed under the Affero General Public License   #
+#                  Version 3 (AGPLv3)                             #
+#                                                                 #
+# You should have received a copy of the GNU Affero General       #
+# Public License along with this program. If not, see             #
+# <https://www.gnu.org/licenses/agpl-3.0.html>.                   #
+#                                                                 #
+# Author: thegexi@gmail.com                                       #
+###################################################################
+
 from lib.scanner import Scanner
 from lib.error import Checker
 from lib.http import HTTP
