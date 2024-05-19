acc7da855b852d0bbeb7fb702ace3bb4e5180476
Miguel Álvarez <128592227+malvads@users.noreply.github.com>
2024-05-20 00:15:05 +0100

    Bump setup to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sqlmc',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     package_data={'': ['VERSION']},
     include_package_data=True,
